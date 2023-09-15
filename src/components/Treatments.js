f3a00aaf139c159113795f07ecaf875caf933054
Lucajett99 <lucagenova99@gmail.com>
2023-09-15 21:31:08 +0200

    treatments update

--- src/components/Treatments.js
+++ src/components/Treatments.js
@@ -1,49 +1,107 @@
-const people = [
+import {
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  FingerPrintIcon,
+  LockClosedIcon,
+} from "@heroicons/react/24/outline";
+
+import Image from "next/image";
+import speechImage from "../../public/speech2.svg";
+import learningImage from "../../public/learning.svg";
+
+const features = [
   {
-    name: "Leslie Alexander",
-    role: "Co-Founder / CEO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    name: "Ritardi e disturbi del linguaggio",
+    description:
+      "Valutazione e interventi per migliorare la comunicazione nei bambini con difficoltà linguistiche.",
+    icon: (
+      <Image
+        src={speechImage}
+        className="h-16 w-16 mx-auto text-indigo-600"
+        aria-hidden="true"
+      />
+    ),
   },
-  // More people...
+  {
+    name: "Valutazione e trattamento dei disturbi dell’apprendimento",
+    description:
+      "Supporto personalizzato per affrontare sfide nell'apprendimento e nello sviluppo cognitivo.",
+    icon: (
+      <Image
+        src={learningImage}
+        className="h-16 w-16 mx-auto text-indigo-600"
+        aria-hidden="true"
+      />
+    ),
+  },
+  {
+    name: "Valutazione e trattamento della deglutizione atipica e disfunzionale",
+    description:
+      "Aiuto per affrontare problemi di deglutizione irregolare o problematica.",
+    icon: (
+      <Image
+        src={learningImage}
+        className="h-16 w-16 mx-auto text-indigo-600"
+        aria-hidden="true"
+      />
+    ),
+  },
 ];
+
+/*
+
+  
+  {
+    name: "Disturbi della fluenza (balbuzie)",
+    description:
+      "Terapia per migliorare la fluidità nella comunicazione verbale.",
+    icon: FingerPrintIcon,
+  },
+  {
+    name: "Disturbi della voce",
+    description:
+      "Interventi per preservare o ripristinare la salute e la qualità della voce.",
+    icon: FingerPrintIcon,
+  },
+  {
+    name: "Esiti post-ictus (afasia)",
+    description:
+      "Supporto per la riabilitazione della comunicazione dopo ictus, compresa l'afasia.",
+    icon: FingerPrintIcon,
+  },
+*/
 
 export default function Treatments() {
   return (
-      <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
-        <div className="max-w-2xl">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Meet our leadership
-          </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae
-            elementum enim vitae ullamcorper suspendisse.
-          </p>
-        </div>
-        <ul
-          role="list"
-          className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
-        >
-          {people.map((person) => (
-            <li key={person.name}>
-              <div className="flex items-center gap-x-6">
-                <img
-                  className="h-16 w-16 rounded-full"
-                  src={person.imageUrl}
-                  alt=""
-                />
-                <div>
-                  <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
-                    {person.name}
-                  </h3>
-                  <p className="text-sm font-semibold leading-6 text-indigo-600">
-                    {person.role}
-                  </p>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+    <div className="relative overflow-hidden px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+      <div className="mx-auto max-w-2xl lg:text-center">
+        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Di cosa mi occupo?
+        </p>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Questi sono i trattamenti che utilizzo per picchiare i bambini
+        </p>
       </div>
+      <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+          {features.map((feature) => (
+            <div
+              key={feature.name}
+              className="text-center bg-slate-300 rounded-lg shadow-lg"
+            >
+              <div className="p-6">
+                {feature.icon}
+                <p className="mt-4 text-base font-semibold text-gray-900">
+                  {feature.name}
+                </p>
+                <p className="mt-2 text-base text-gray-600">
+                  {feature.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
