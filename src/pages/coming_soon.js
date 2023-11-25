173d72a89ce2a3708f27b6e1372c4392f4dced20
Lucajett99 <lucagenova99@gmail.com>
2023-11-25 16:15:08 +0100

    bug fixed

--- src/pages/coming_soon.js
+++ src/pages/coming_soon.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/*import React, { useState, useEffect } from "react";
 
 export default function coming_soon({ children }) {
     const [dot, setDot] = useState(".");
@@ -8,6 +8,37 @@ export default function coming_soon({ children }) {
         }, 500);
         return () => clearInterval(interval);
     }, [dot]);
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-blue-300 to-purple-500 flex justify-center items-center">
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            Sito in costruzione <br />
+            <span className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+              {dot}
+            </span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+*/
+import React, { useState, useEffect } from "react";
+
+export default function ComingSoon({ children }) {
+  const [dot, setDot] = useState(".");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDot((currentDot) =>
+        currentDot === "." ? ".." : currentDot === ".." ? "..." : "."
+      );
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []); // Rimuovi "dot" dalle dipendenze di useEffect
+
   return (
     <div className="min-h-screen bg-gradient-to-r from-blue-300 to-purple-500 flex justify-center items-center">
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
